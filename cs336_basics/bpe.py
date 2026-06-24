da276187c45e0d6c5293ffaa6a3aab135ec74ea5
wooloo_lover <wooloo.loverr@gmail.com>
2026-06-24 21:35:51 +0800

    完成naive版本的train bpe

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -78,18 +78,90 @@ def count_pretokens(chunk_text: str, special_tokens: list[str])->dict[tuple[byte
 
     return pretoken_counts
 
+def init_vocab(special_tokens: list[str])->dict[int, bytes]:
+    """
+    1. 创建256个基础byte token
+    2. 把special_tokens追加进vocab
+    4. 返回dict[int, bytes]
+    """
+    vocab: dict[int, bytes] = {}
+    for i in range(256):
+        vocab[i] = bytes([i])
+    for special_token in special_tokens:
+        vocab[len(vocab)] = special_token.encode('utf-8')
+
+    return vocab
+
+def count_adjacent_pairs(pretoken_counts:dict[tuple[bytes, ...], int])->dict[tuple[bytes, bytes], int]:
+    """
+    1. 统计输入的dict的每个pretoken内部相邻bytes的出现频率
+    2. 返回bytes-pair出现频率字典
+    """
+    
+    pair_counts: dict[tuple[bytes, bytes], int] = {}
+    for pretoken,count in pretoken_counts.items():
+        if len(pretoken) >=2:
+            for i in range(len(pretoken) - 1):
+                cur_pair = (pretoken[i], pretoken[i + 1])
+                pair_counts[cur_pair] = pair_counts.get(cur_pair, 0) + count
+
+    return pair_counts
+
+def select_best_pair(pair_counts: dict[tuple[bytes, bytes], int])->tuple[bytes, bytes]:
+    """
+    1. 选择输入的dict的出现最高的bytes-pair
+    2. 返回出现最高的bytes-pair
+    """
+    best_pair: tuple[bytes, bytes] | None = None
+    best_count = -1
+
+    for pair, count in pair_counts.items():
+        if best_pair is None or count > best_count or (count == best_count and pair > best_pair):
+            best_pair = pair
+            best_count = count
+
+    assert best_pair is not None
+    
+    return best_pair
+
+def merge_pair_in_pretokens(pretoken_counts: dict[tuple[bytes, ...], int], pair_to_merge: tuple[bytes, bytes])->dict[tuple[bytes, ...], int]:
+    """
+    1. 遍历每个pretoken，把其中出现的pair合并
+    2. 如果合并以后和之前的pretoken重复了，就把count加起来
+    """ 
+    merged_pretoken_counts: dict[tuple[bytes, ...], int] = {}
+    for pretoken, count in pretoken_counts.items():
+        merged_tokens = []
+        pretoken_lens = len(pretoken)
+        i = 0
+        while i < pretoken_lens:
+            if i+1 <pretoken_lens and ((pretoken[i], pretoken[i + 1]) == pair_to_merge):
+                merged_tokens.append(pair_to_merge[0] + pair_to_merge[1])
+                i += 2
+            else:
+                merged_tokens.append(pretoken[i])
+                i += 1
+        merged_pretoken = tuple(merged_tokens)
+        merged_pretoken_counts[merged_pretoken] = merged_pretoken_counts.get(merged_pretoken, 0) + count
+        
+    return merged_pretoken_counts
+
+
 
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]: 
     """
     1. 先把给定文本分成若干大小不一的chunk，然后对每个chunk可以并行处理了。
     2. 预处理每个chunk，执行pre-tokenizaion
     3. 汇总所有的chunk的pre-tokenization的字节tuple的频率统计结果
-    4. TODO：随着代码完成继续补充
-    6. 
+    4. 创建初始词汇表和merges
+    5. 开始 merge loop
+    5.1. 计算pretoken内部相邻bytes的出现个数
+    5.2. 计算出现频率最高的token pair
+    5.3. 把出现频率最高的token pair合并，并且记录merge结果，并且写入vocab
+    5.4. 循环以上三步直到达到最大的次数
     """
 
     with open(input_path, 'rb') as f:
-
         # 分 chunk
         num_processes = 4
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
@@ -103,17 +175,33 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]) -> tu
             for byte_tuple, count in chunk_pretoken_counts.items():
                 total_pretoken_counts[byte_tuple] = total_pretoken_counts.get(byte_tuple, 0) + count
 
-    # TODO:验证“分 chunk 后汇总”与“整个文件一次性调用 count_pretokens”得到的字典完全相等。用一个较小、包含多个 <|endoftext|> 的文本测试，这能验证分块没有改变预分词结果。
+    # 创建初始词汇表
+    vocab = init_vocab(special_tokens)
+    merges: list[tuple[bytes, bytes]] = []
+
+    while len(vocab) < vocab_size:
+        # 计算相邻token pair的频率
+        pair_counts = count_adjacent_pairs(total_pretoken_counts)
+
+        if len(pair_counts) == 0:
+            break
+
+        # 计算出现频率最高的pair
+        best_pair = select_best_pair(pair_counts)
+        vocab[len(vocab)] = best_pair[0] + best_pair[1]
+        merges.append(best_pair)
+
+        # 在pretoken中把出现频率最高的token pair合并
+        total_pretoken_counts = merge_pair_in_pretokens(total_pretoken_counts, best_pair)
+
+    return vocab, merges
 
 if __name__ == '__main__':
     # do test
 
-    small_text = "<|endoftext|>abc<|endoftext|><|endoftext|>def<|endoftext|>"
+    vocab, merges = train_bpe('data/TinyStoriesV2-GPT4-valid.txt', 256 + 1 + 10, ["<|endoftext|>"])
 
-    pretoken_dict = count_pretokens(small_text, ["<|endoftext|>"])
-
-    for key, value in pretoken_dict.items():
-        print(key, value)
-        print(b"".join(key))
-
-    
+    print(len(vocab))
+    print(len(merges))
+    print(merges)
+    print(list(vocab.items())[-10:])
